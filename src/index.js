f1ca1e5362e640f6b8277aaff27ba26753d8a78d
Carrington Perry <carringtonperry@Carringtons-MacBook-Pro.local>
2018-08-28 15:48:16 -0400

    commit before debugger

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
+
+import reducers from "./reducers";
+import PostsIndex from "./components/posts-index";
+import PostsShow from "./components/posts-show";
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/" component={PostsIndex} />
+          <Route path="/posts/:id" component={PostsShow} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);

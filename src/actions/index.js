751c6781981e650e5afdb9fd85ea5ce3f892cf65
Carrington Perry <carringtonperry@Carringtons-MacBook-Pro.local>
2018-08-29 11:06:23 -0400

    finished part 1 and 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
 export const FETCH_POST = "fetch_post";
+export const DELETE_POST = "delete_post";
+export const CREATE_POST = "create_post"
+
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=PAPERCLIP1234";
@@ -21,5 +24,27 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios
+    .delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+    .then(() => callback());
+
+  return {
+    type: DELETE_POST,
+    payload: id
+  };
+}
+
+export function createPost(values,callback) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
+  request.then(()=>callback());
+
+  return {
+    type: CREATE_POST,
+    payload: request
+
   };
 }
--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
-import { fetchPost } from "../actions";
+import { fetchPost, deletePost } from "../actions";
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
   }
 
   render() {
@@ -18,18 +26,27 @@ class PostsShow extends Component {
 
     return (
       <div>
-        <h1>Show a single post</h1>
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
 }
 
-function mapStateToProps({ posts }, ownProps) {
-  return { post: posts[ownProps.match.params.id] };
+function mapStateToProps(state, ownProps) {
+  return { post: state.posts[ownProps.match.params.id] };
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({fetchPost}, dispatch);
+  return bindActionCreators({fetchPost, deletePost}, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostsShow);
